6104a86d60ac059a03573d90ed0e2b64771c9cd0
Erin Platvoet <erin.didomenico520@gmail.com>
2020-08-21 18:27:33 -0400

    logic set up in Note class

--- db/notes.js
+++ db/notes.js
@@ -3,3 +3,25 @@ const util = require("util");
 const uuid = require("uuid/v1");
 
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Note {
+    //readNotes(){
+        //
+    //}
+    //writeNote(){
+        //
+    //}
+    //getNotes(){
+        //
+    //}
+    //saveNotes(){
+        //
+    //}
+    //deleteNotes(){
+        //
+    // }
+}
+
+module.exports = new Note();
